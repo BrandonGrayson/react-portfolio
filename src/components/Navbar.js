5e7ce1758b085549a783889f9be23ed4953f692d
Brandon <brandonbgrayson@gmail.com>
2021-02-07 13:20:26 -0500

    added name to navbar preparing to switch to new branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 	return (
 		<nav class="navbar navbar-dark bg-dark">
 			<div class="container-fluid">
-				<span class="navbar-brand mb-0 h1">Navbar</span>
+				<span class="navbar-brand mb-0 h1">Brandon Grayson</span>
 			</div>
 		</nav>
 	);
