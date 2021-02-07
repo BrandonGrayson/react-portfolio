d04ba98eedd62298d207b91b5038760b4b491696
Brandon <brandonbgrayson@gmail.com>
2021-02-07 13:10:58 -0500

    imported nav skeleton and bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 // dependencies
-import React from "react";
-
+import React from 'react';
 
 const Navbar = () => {
-    
-}
+	return (
+		<nav class="navbar navbar-dark bg-dark">
+			<div class="container-fluid">
+				<span class="navbar-brand mb-0 h1">Navbar</span>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar
