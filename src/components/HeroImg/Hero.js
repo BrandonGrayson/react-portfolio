f76e7160bc4934de123dd54bee7c1c95b27cf069
Brandon <brandonbgrayson@gmail.com>
2021-02-07 14:50:11 -0500

    cleared up className err/ added bootstrap grid system

--- src/components/HeroImg/Hero.js
+++ src/components/HeroImg/Hero.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 const Hero = () => {
 	return (
-		<div class="card" style="width: 18rem;">
-			<div class="card-body">
-				<h5 class="card-title">Uniquely Crafted Web Experiences</h5>
-				<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-				<p class="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card's content.
-				</p>
-				<a href="#" class="card-link">
-					Card link
-				</a>
-				<a href="#" class="card-link">
-					Another link
-				</a>
+		<div className="Container">
+			<div className="row">
+				<div className='col-xs-12 col-sm-12 col-md-8 col-lg-8'>
+					<div className="card">
+						<div className="card-body">
+							<h5 className="card-title">Uniquely Crafted Web Experiences</h5>
+							<h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+							<p class="card-text">Meet Brandon Grayson</p>
+							<a href="#" className="card-link">
+								Card link
+							</a>
+							<a href="#" className="card-link">
+								Another link
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 };
 
-export default Hero
+export default Hero;
