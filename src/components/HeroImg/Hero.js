eadf36538dd26105bca27d6070dc733437467386
Brandon <brandonbgrayson@gmail.com>
2021-02-07 20:40:06 -0500

    hero img height adjusted and content centered

--- src/components/HeroImg/Hero.js
+++ src/components/HeroImg/Hero.js
@@ -3,29 +3,24 @@ import "./styles.css"
 
 
 const styles = {
-    hero: {
-        height: "100vh"
-    },
     card: {
-        background: "red"
+        background: "red",
+        height: "60vh"
     }
 }
 
 const Hero = () => {
 	return (
-		<div className="Container Jumbotron" style={styles.hero}>
+		<div className="Container">
 			<div className="row">
 				<div className='col-xs-12 col-sm-12 col-md-8 col-lg-8'>
 					<div className="card" style={styles.card}>
-						<div className="card-body">
+						<div className="card-body d-flex flex-column justify-content-center align-items-center">
 							<h5 className="card-title">Uniquely Crafted Web Experiences</h5>
 							<h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-							<p class="card-text">Meet Brandon Grayson</p>
+							<p className="card-text">Meet Brandon Grayson</p>
 							<a href="#" className="card-link">
 								Card link
-							</a>
-							<a href="#" className="card-link">
-								Another link
 							</a>
 						</div>
 					</div>
